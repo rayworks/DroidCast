31606dabfa8000bb1efcee729cdd4cdb33cb0528
rayworks <crayzhou@gmail.com>
2020-11-25 00:14:30 +0800

    Retrieve apk path by  'popen' function instead of raw 'pipe' call

--- cmd_tool/cmd_runner.c
+++ cmd_tool/cmd_runner.c
@@ -26,7 +26,7 @@ static const char *remote = "/data/local/tmp/";
 
 #define ARRAY_LEN(a) (sizeof(a) / sizeof(a[0]))
 
-#define BUF_SIZE 1024*2
+#define BUF_SIZE 1024 * 2
 
 // NB: Give that the target apk/dex file has been pushed into '/data/local/tmp/' on the device, it's
 // still possible to get an error about "could not find class 'com.rayworks.droidcast.Main'". so we
@@ -171,7 +171,9 @@ static void handler(int sig)
 
         pid_t proc = adb_execute(NULL, un_fwd_cmd, ARRAY_LEN(un_fwd_cmd));
         wait_for_child_process(proc, "adb undo forward");
-    } else if (sig == SIGALRM) {
+    }
+    else if (sig == SIGALRM)
+    {
         signal(SIGALRM, SIG_DFL);
 
         system("open http://localhost:53516/screenshot");
@@ -182,100 +184,84 @@ static void handler(int sig)
 
 // Use a pipe to communicate between a parent and child process (via Android Package Manager) for
 // retrieving the actual installed apk path.
-void retrieve_src_apk_path()
+static int resovle_apk_path()
 {
-    int fd[2];
-    pid_t pid;
+    char cmd[BUF_SIZE];
+    const char *cmd_fmt = "%s shell pm path com.rayworks.droidcast";
+    snprintf(cmd, BUF_SIZE, cmd_fmt, get_adb_command());
 
-    char line[MAXLINE];
+    FILE *pfin;
+    if ((pfin = popen(cmd, "r")) == NULL)
+        err_sys("popen error");
 
-    if (signal(SIGPIPE, sig_pipe) == SIG_ERR) {
-        err_sys("signal error");
+    int fd = fileno(pfin);
+
+    char buffer[BUF_SIZE];
+    char result[BUF_SIZE];
+    memset(result, 0, BUF_SIZE);
+
+    int readCnt = 0;
+    while (1)
+    {
+        ssize_t count = read(fd, buffer, sizeof(buffer));
+        printf("bytes %ld read from pipe\n", count);
+
+        if (count == -1)
+        {
+            if (errno == EINTR)
+            {
+                continue;
+            }
+            else
+            {
+                perror("read");
+                exit(1);
+            }
+        }
+        else if (count == 0)
+        {
+            break;
+        }
+        else
+        {
+            // NB: the read content could be discontinued, so gather all the content first
+            char *ptr = result;
+
+            strncpy(ptr + readCnt, buffer, count);
+            readCnt += count;
+        }
     }
 
-    if (pipe(fd) < 0)
-        err_sys("pipe error");
+    if (pclose(pfin) == -1)
+        err_sys("pclose err");
 
-    if ((pid = fork()) < 0) {
-        err_sys("fork error");
-    } else if (pid > 0) { //parent
-        close(fd[1]); // out closed for parent
-
-        char buffer[BUF_SIZE];
-
-        char result[BUF_SIZE];
-        memset(result, 0, BUF_SIZE);
-
-        int readCnt = 0;
-        while (1) {
-            ssize_t count = read(fd[0], buffer, sizeof (buffer));
-            printf("bytes %ld read from pipe\n", count);
-
-            if (count == -1) {
-                if (errno == EINTR) {
-                    continue;
-                } else {
-                    perror("read");
-                    exit(1);
-                }
-            } else if (count == 0) {
-                break;
-            } else {
-                // NB: the read content could be discontinued, so gather all the content first
-                char* ptr = result;
-
-                strncpy(ptr + readCnt, buffer, count);
-                readCnt += count;
-            }
+    if (readCnt > 0)
+    {
+        // format like:
+        // package:/data/app/com.rayworks.droidcast-Tb1-e8DHFvuQ1wI6_MlLww==/base.apk
+        apk_src_path = filter_apk_path(result);
+        if (!apk_src_path)
+        {
+            err_msg("Fatal error: Apk path can't be retrieved, Have you installed the app successfully?");
+            exit(-1);
         }
 
-        if (readCnt > 0) {
-            // format like:
-            // package:/data/app/com.rayworks.droidcast-Tb1-e8DHFvuQ1wI6_MlLww==/base.apk
-            apk_src_path = filter_apk_path(result);
-            if(!apk_src_path){
-                err_msg("Fatal error: Apk path can't be retrieved, Have you installed the app successfully?");
-                exit(-1);
-            }
-
-            printf("Target path is : %s\n", apk_src_path);
-        }
-
-        close(fd[0]);
-
-        wait(NULL);
-
-    } else {
-        if (fd[1] != STDOUT_FILENO) {
-            if (dup2(fd[1], STDOUT_FILENO) != STDOUT_FILENO) {
-                err_sys("dup2 error to stdout");
-            }
-            close(fd[1]);
-            close(fd[0]);
-        }
-
-        const char *const cmd[] = {
-                    "shell",
-                    "pm",
-                    "path",
-                    "com.rayworks.droidcast"
-                };
-         adb_execute(NULL, cmd, ARRAY_LEN(cmd));
-         wait(NULL);
-
-         printf("child proc existing\n");
-
-         exit(EXIT_SUCCESS);
+        printf("Target path is : %s\n", apk_src_path);
     }
+
+    return readCnt;
 }
 
-void sleep_ext(unsigned int seconds, void (*func)(int)) {
-    if(signal(SIGALRM, func) == SIG_ERR) {
+void sleep_ext(unsigned int seconds, void (*func)(int))
+{
+    if (signal(SIGALRM, func) == SIG_ERR)
+    {
         perror("error : signal SIGALRM");
         exit(EXIT_FAILURE);
     }
 
-    if(setjmp(env_alarm) == 0) {
+    if (setjmp(env_alarm) == 0)
+    {
         alarm(seconds);
         pause(); /* next caught signal will wake this up */
     }
@@ -283,21 +269,29 @@ void sleep_ext(unsigned int seconds, void (*func)(int)) {
 
 int main(int argc, char *argv[])
 {
-    retrieve_src_apk_path();
-    
+    if (resovle_apk_path() == 0)
+    {
+        perror("Apk not found, exit now\n");
+        exit(-1);
+    };
+
     pid_t proc = adb_execute(NULL, fwd_cmd, ARRAY_LEN(fwd_cmd));
     wait_for_child_process(proc, "adb forward");
 
     char class_path[256];
-    if(apk_src_path) {
+    if (apk_src_path)
+    {
         snprintf(class_path, sizeof(class_path), "CLASSPATH=%s", apk_src_path);
-    }else {
+    }
+    else
+    {
         snprintf(class_path, sizeof(class_path), "CLASSPATH=%s%s", remote, apkname);
     }
     printf("> full class path: %s\n", class_path);
 
     // setup the handler for monitoring the core child process quitting
-    if(signal(SIGCHLD, handler) == SIG_ERR) {
+    if (signal(SIGCHLD, handler) == SIG_ERR)
+    {
         perror("error: signal SIGCHLD");
         exit(EXIT_FAILURE);
     }
