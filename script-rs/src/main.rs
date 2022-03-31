941ddafcd67b6ff2c3844dbb2d974345ff269f68
rayworks <crayzhou@gmail.com>
2022-03-31 18:19:52 +0800

    Update Rust script to support the specified device if multiple ones connected

--- script-rs/src/main.rs
+++ script-rs/src/main.rs
@@ -12,8 +12,8 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     println!(">>> args {:?}", args);
 
-    if args.len() != 2 {
-        println!("usage : prog [port]");
+    if args.len() != 2 && args.len() != 3 {
+        println!("usage : prog port [serial number]");
         return;
     }
 
@@ -21,29 +21,22 @@ fn main() {
     port.trim().parse::<u32>().expect("The port must be a number.");
 
     // adb devices
-    let devices_result = Command::new("adb").arg("devices").output();
-    let s = devices_result.unwrap().stdout;
-    let devices_out = std::str::from_utf8(&s).unwrap();
-    let dev_cnt = devices_out.matches("device").count();
+    let dev_cnt = count_connected_devices();
     if dev_cnt < 2 {
         println!("Make sure your device is connected");
         return;
+    } else if dev_cnt > 2 {
+        if args.len() < 3 {
+            println!("Multiple devices connected, please specify the target device serial number");
+            return;
+        }
     }
 
     // apk path
-    let params = vec!["shell", "pm", "path", "com.rayworks.droidcast"];
-    let path_result = Command::new("adb").args(params).output();
-    let path = path_result.unwrap().stdout;
-
-    let classpath = String::from("CLASSPATH=");
-    let mut raw_path = std::str::from_utf8(&path).unwrap();
-    raw_path = raw_path.split(":").last().unwrap().trim();
-    if raw_path.is_empty() {
-        println!("Apk not found, have you installed it successfully?");
+    let full_path = locate_apk_path();
+    if full_path.is_empty() {
         return;
     }
-    let full_path = classpath.to_string() + &(raw_path.to_string());
-    println!("Path {}", full_path);
 
     // forward
     forward_connection(&port);
@@ -60,6 +53,41 @@ fn main() {
     println!("About to quit the app");
 }
 
+fn serial_checked_command() -> Command {
+    let mut cmd = Command::new("adb");
+    let args: Vec<String> = env::args().collect();
+    if args.len() >= 3 {
+        cmd.args(vec!["-s", &args[2]]);
+    }
+    cmd
+}
+
+fn count_connected_devices() -> usize {
+    let devices_result = Command::new("adb").arg("devices").output();
+    let s = devices_result.unwrap().stdout;
+    let devices_out = std::str::from_utf8(&s).unwrap();
+
+    println!("{}", format!("\nDevices info : {}", devices_out));
+    devices_out.matches("device").count()
+}
+
+fn locate_apk_path() -> String {
+    let params = vec!["shell", "pm", "path", "com.rayworks.droidcast"];
+    let path_result = serial_checked_command().args(params).output();
+    let path = path_result.unwrap().stdout;
+
+    let mut raw_path = std::str::from_utf8(&path).unwrap();
+    raw_path = raw_path.split(":").last().unwrap().trim();
+    if raw_path.is_empty() {
+        eprintln!("Apk not found, have you installed it successfully?");
+        return "".to_string();
+    }
+    let full_path = String::from("CLASSPATH=") + &(raw_path.to_string());
+    println!("Path {}", full_path);
+
+    full_path
+}
+
 fn startup_service_and_wait(port: &String, full_path: String) {
     // app_process
     let port_param = String::from("--port=") + &port;
@@ -73,7 +101,7 @@ fn startup_service_and_wait(port: &String, full_path: String) {
     ];
     println!("Params -> {:?}", params);
 
-    let result = Command::new("adb").args(params)
+    let result = serial_checked_command().args(params)
         .spawn()
         .unwrap()
         .wait_with_output()
@@ -87,14 +115,14 @@ fn forward_connection(port: &String) {
     let params_fwd = vec!["forward", &grp.trim(), &grp.trim()];
     println!("Params -> {:?}", params_fwd);
 
-    Command::new("adb").args(params_fwd).output().expect("Failed to forward the tcp connection");
+    serial_checked_command().args(params_fwd).output().expect("Failed to forward the tcp connection");
 }
 
 fn unforward_connection(port: &String) {
     let tcp = format!("tcp:{}", &port);
     let params_fwd = vec!["forward", "--remove", &tcp];
 
-    let status = Command::new("adb").args(params_fwd).output().unwrap().status;
+    let status = serial_checked_command().args(params_fwd).output().unwrap().status;
     println!("adb unforward action status : {:?}", status);
 }
 
@@ -114,7 +142,7 @@ fn open_browser() {
     let args: Vec<String> = env::args().collect();
 
     let ip_param = vec!["shell", "ip route | awk '/wlan*/{ print $9 }'| tr -d '\n'"];
-    let ip = Command::new("adb").args(ip_param).output().unwrap().stdout;
+    let ip = serial_checked_command().args(ip_param).output().unwrap().stdout;
     let ip = std::str::from_utf8(&ip).unwrap().to_string();
     println!(">>> Share the url 'http://{}:{}/screenshot' to see the live screen", ip, args[1]);
 
